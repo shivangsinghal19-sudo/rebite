c2f8a92d3c59b631fc79fc86800997ab2c85416e
Shivang <shivang@Shivangs-MacBook-Air-2.local>
2026-03-01 11:51:08 +0530

    Fix PWA plugin properly

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+      manifest: {
+        name: 'Rebite',
+        short_name: 'Rebite',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -11,12 +40,4 @@ export default defineConfig({
   server: {
     port: 5173,
   },
-})
-
-VitePWA({
-  registerType: 'autoUpdate',
-  workbox: {
-    skipWaiting: true,
-    clientsClaim: true,
-  }
 })
